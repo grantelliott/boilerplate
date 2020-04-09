7dae54c6bd98d03699a5d20b5860bd77411e57a5
Grant Elliott <grant.elliott@arnoldclark.com>
2020-04-09 14:39:48 +0100

    Update postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,9 @@
-// postcss.config.js
 const purgecss = require('@fullhuman/postcss-purgecss')({
 
   // Specify the paths to all of the template files in your project
   content: [
     './src/**/*.html',
-    './src/**/*.vue',
-    './src/**/*.jsx',
-    // etc.
+    './src/**/*.njk',
   ],
 
   // Include any special characters you're using in this regular expression
